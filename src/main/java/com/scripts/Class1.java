355356387f080974f43003b6b24117bd70c94e47
sumeet.agrawal <sumeet.agrawal@BDC8-DX-352STXG.dir.svc.accenture.com>
2017-08-09 15:43:05 +0530

    Add java test

---
+++ src/main/java/com/scripts/Class1.java
@@ -0,0 +1,14 @@
+package com.scripts;
+
+public class Class1 {
+	
+	public static void main(String[]  args) {
+		
+		System.out.println("hello");
+		
+		Class2 a = new Class2();
+		System.out.println(a.method(3));
+		
+	}
+
+}
---
+++ src/main/java/com/scripts/Class2.java
@@ -0,0 +1,8 @@
+package com.scripts;
+
+public class Class2 {
+
+	public int method( int i) {
+		return 2;
+	}
+}
