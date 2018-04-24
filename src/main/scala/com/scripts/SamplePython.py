a6e9529d5dc0a857afcd4d905179acd560a79a5d
sumeet agrawal <sumeet.agrawal@microsoft.com>
2018-04-24 12:44:48 +0530

    Refactored the python code

--- src/main/scala/com/scripts/SamplePython.py
+++ src/main/scala/com/scripts/SamplePython.py
@@ -1,8 +1,8 @@
-package com.scripts
+#package com.scripts
 
-object SamplePython {
+#object SamplePython {
   
-/*  import argparse
+import argparse
 
 class sumeet_class(object):
     def __init__(self,name,balance,bank_name):
@@ -34,26 +34,26 @@ class sumeet_class(object):
 def main(argv= None):
 
         parser = argparse.ArgumentParser()
-        parser.add_argument("-name","--name",choices=["sumeet"],
-                            help = "name of the person")
+        parser.add_argument("-name", "--name", choices=["sumeet"],
+                            help="name of the person")
 
-        parser.add_argument("-balance","--balance",choices=[10,20,30],
+        parser.add_argument("-balance", "--balance", choices=[10, 20, 30],
                             type = int, help = "Initial balance")
 
-        parser.add_argument("-bank1",choices=["JPM","HDFC","AXIS"], dest = "bank_name",
-                            help = "name of the person",default= False)
+        parser.add_argument("-bank1", choices=["JPM", "HDFC", "AXIS"], dest="bank_name",
+                            help="name of the person", default=False)
 
         args = parser.parse_args() if not  argv else parser.parse_args(argv)
         print(args)
-        Sumeet_obj = sumeet_class(**vars(args))
-        print(Sumeet_obj.withdraw(10))
+        sumeet_obj = sumeet_class(**vars(args))
+        print(sumeet_obj.withdraw(10))
         #print("Bank_name==============="+ args.bank_name)
-        if not Sumeet_obj.bank_return():
+        if not sumeet_obj.bank_return():
             print("noooo")
         else:
             print("Yessssss")
 
 if __name__ == "__main__":
-    main()  */
+     main()
   
-}
+#}
